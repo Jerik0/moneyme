f13be5fa5d708f671694c90ceab874e4df3b68c7
jerik0 <jcarrico1985@live.com>
2018-10-11 15:55:47 -0500

    added billrepository complete with query for getting current bills for the month

--- src/main/java/com/budgeting/moneyme/controllers/BillsController.java
+++ src/main/java/com/budgeting/moneyme/controllers/BillsController.java
@@ -1,8 +1,18 @@
 package com.budgeting.moneyme.controllers;
 
+import com.budgeting.moneyme.models.Bill;
 import com.budgeting.moneyme.repositories.BillRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.*;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Locale;
 
 @Controller
 public class BillsController {
@@ -11,6 +21,45 @@ public class BillsController {
 
     @Autowired
     public BillsController(BillRepository billsDao) {
-        this.billsDao = billsDao;
+      this.billsDao = billsDao;
+    }
+
+    @GetMapping("/bills")
+    public String bills(Model model) {
+      Instant instant = Instant.now();
+      DateTimeFormatter formatter =
+          DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT)
+              .withLocale(Locale.US)
+              .withZone(ZoneId.systemDefault());
+      String timestamp = formatter.format(instant);
+      int month = Integer.parseInt(timestamp.substring(0,2)); //retrieve current month
+      int year = Integer.parseInt(timestamp.substring(6,8));
+      int date;
+      int nextMonth;
+
+      if(month == 12) { //There is no 13th month, therefore we start over at 1.
+        nextMonth = 1;
+      } else {
+        nextMonth = month + 1;
+      }
+
+      if(month >= 10) { //date is further up or further down the string depending on a 1 or 2 digit month.
+        date = Integer.parseInt(timestamp.substring(3,5));
+      } else {
+        date = Integer.parseInt(timestamp.substring(2,4));
+      }
+
+      System.out.println("date: " + date);
+
+      List<Bill> currentBills = billsDao.findCurrentBills(date);
+
+      model.addAttribute("timestamp", timestamp);
+      model.addAttribute("nextMonth", nextMonth);
+      model.addAttribute("month", month);
+      model.addAttribute("date", date);
+      model.addAttribute("year", year);
+      model.addAttribute("bills", billsDao.findAll());
+      model.addAttribute("currentBills", currentBills);
+      return "/index";
     }
 }
--- src/main/java/com/budgeting/moneyme/repositories/BillRepository.java
+++ src/main/java/com/budgeting/moneyme/repositories/BillRepository.java
@@ -3,7 +3,14 @@ package com.budgeting.moneyme.repositories;
 import com.budgeting.moneyme.models.Bill;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import java.util.List;
 
 public interface BillRepository extends CrudRepository<Bill, Long> {
-    Bill findByName(String name);
+  Bill findByName(String name);
+
+  @Query(
+      nativeQuery = true,
+      value = "SELECT * FROM bills b WHERE b.date >= ?1 ORDER BY date"
+  )
+  List<Bill> findCurrentBills(int date);
 }
