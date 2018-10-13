18aba8955f66138ff7cdb954226cfcb2b0cfaa21
Jonathan Carrico <jcarrico@cosential.com>
2018-10-13 18:12:30 -0500

    added more stuff. lazy commit message

--- src/main/java/com/budgeting/moneyme/controllers/BillsController.java
+++ src/main/java/com/budgeting/moneyme/controllers/BillsController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.*;
 import java.time.Instant;
@@ -17,47 +19,57 @@ import java.util.Locale;
 @Controller
 public class BillsController {
 
-    private final BillRepository billsDao;
+  private final BillRepository billsDao;
 
-    @Autowired
-    public BillsController(BillRepository billsDao) {
-      this.billsDao = billsDao;
+  @Autowired
+  public BillsController(BillRepository billsDao) {
+    this.billsDao   = billsDao;
+  }
+
+  @GetMapping("/bills")
+  public String bills(Model model) {
+    Instant instant = Instant.now();
+    DateTimeFormatter formatter =
+        DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT)
+            .withLocale(Locale.US)
+            .withZone(ZoneId.systemDefault());
+    String timestamp = formatter.format(instant);
+    int month = Integer.parseInt(timestamp.substring(0,2)); //retrieve current month
+    int year = Integer.parseInt(timestamp.substring(6,8));
+    int date;
+    int nextMonth;
+
+    if(month == 12) { //There is no 13th month, therefore we start over at 1.
+      nextMonth = 1;
+    } else {
+      nextMonth = month + 1;
     }
 
-    @GetMapping("/bills")
-    public String bills(Model model) {
-      Instant instant = Instant.now();
-      DateTimeFormatter formatter =
-          DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT)
-              .withLocale(Locale.US)
-              .withZone(ZoneId.systemDefault());
-      String timestamp = formatter.format(instant);
-      int month = Integer.parseInt(timestamp.substring(0,2)); //retrieve current month
-      int year = Integer.parseInt(timestamp.substring(6,8));
-      int date;
-      int nextMonth;
-
-      if(month == 12) { //There is no 13th month, therefore we start over at 1.
-        nextMonth = 1;
-      } else {
-        nextMonth = month + 1;
-      }
-
-      if(month >= 10) { //date is further up or further down the string depending on a 1 or 2 digit month.
-        date = Integer.parseInt(timestamp.substring(3,5));
-      } else {
-        date = Integer.parseInt(timestamp.substring(2,4));
-      }
-
-      List<Bill> currentBills = billsDao.findCurrentBills(date);
-
-      model.addAttribute("timestamp", timestamp);
-      model.addAttribute("nextMonth", nextMonth);
-      model.addAttribute("month", month);
-      model.addAttribute("date", date);
-      model.addAttribute("year", year);
-      model.addAttribute("bills", billsDao.findAll());
-      model.addAttribute("currentBills", currentBills);
-      return "/index";
+    if(month >= 10) { //date is further up or further down the string depending on a 1 or 2 digit month.
+      date = Integer.parseInt(timestamp.substring(3,5));
+    } else {
+      date = Integer.parseInt(timestamp.substring(2,4));
     }
+
+    List<Bill> currentBills = billsDao.findCurrentBills(date);
+
+    model.addAttribute("timestamp", timestamp);
+    model.addAttribute("nextMonth", nextMonth);
+    model.addAttribute("month", month);
+    model.addAttribute("date", date);
+    model.addAttribute("year", year);
+    model.addAttribute("bills", billsDao.findAll());
+    model.addAttribute("currentBills", currentBills);
+    return "/index";
+  }
+
+  @GetMapping("/{id}/update")
+  public String update(@PathVariable Long id, Model model) {
+    Bill bill = billsDao.findOne(id);
+    model.addAttribute("bill", bill);
+    return "/update";
+  }
+
+  @PostMapping("/{id}/update/confirm")
+  public String confirmUpdate()
 }
